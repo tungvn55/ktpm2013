d1af5b77a610655c4b3edd0f62af29cff7c96f4d
Vũ Ngọc Tùng <silverblues.274@gmail.com>
2013-09-10 22:59:30 +0700

    File Giai_PTB1Test.java

--- Giai_PTB1Test.java
+++ Giai_PTB1Test.java
@@ -1,8 +1,20 @@
-import junit.framework.TestCase;
+import static org.junit.Assert.*;
 import org.junit.Test;
 
+
 public class Giai_PTB1Test {
+	
+	@Test
 	public void test1() {
-		kq=Giai_PTB1.
+		Giai_PTB1 test = new Giai_PTB1();
+		float kq=test.giai_ptb1(10, -90);
+		assertEquals(9, kq, 0);
+	}
+	
+	@Test
+	public void test2() {
+		Giai_PTB1 test = new Giai_PTB1();
+		float kq=test.giai_ptb1(10, 0);
+		assertEquals(0, kq, 0);
 	}
 }
