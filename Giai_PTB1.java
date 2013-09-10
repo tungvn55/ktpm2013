f6215c918a22506f1f7a42b67c9cf1beef51e742
Vũ Ngọc Tùng <silverblues.274@gmail.com>
2013-09-10 15:37:47 +0700

    Giai_PTB1

---
+++ Giai_PTB1.java
@@ -0,0 +1,7 @@
+﻿public class Giai_PTB1 {
+	public int giai_ptb1(int a, int b) {
+		if (a!=0) {
+			return -b/a;
+		}
+	}
+}
---
+++ Giai_PTB1Test.java
@@ -0,0 +1,8 @@
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class Giai_PTB1Test {
+	public void test1() {
+		kq=Giai_PTB1.
+	}
+}
