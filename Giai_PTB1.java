537a867ac35a91ef45c153487aa8a4dc28882da7
Vũ Ngọc Tùng <silverblues.274@gmail.com>
2013-09-10 22:50:43 +0700

    File Giai_PTB1.java

--- Giai_PTB1.java
+++ Giai_PTB1.java
@@ -1,7 +1,13 @@
-﻿public class Giai_PTB1 {
-	public int giai_ptb1(int a, int b) {
-		if (a!=0) {
-			return -b/a;
+
+public class Giai_PTB1 {
+	public float giai_ptb1(int a, int b) {
+		if(a==0.0 && b==0.0) {
+			throw new IllegalArgumentException("Co vo so nghiem!");
 		}
+		else if(a==0.0) {
+			throw new IllegalArgumentException("Vo nghiem!");
+			}
+			else return (float)-b/(float)a;
+		
 	}
 }
